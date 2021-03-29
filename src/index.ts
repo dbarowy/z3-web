9121ad0641d4eed8acf7d4e6811b3c3cbd7efb8f
Daniel W. Barowy <dbarowy@cs.williams.edu>
2021-03-29 10:37:47 -0400

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import express from "express";
-const app = express();
-const port = 3456;
+import util from "util";
+import { exec } from "child_process";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const exe = util.promisify(exec); // do magic JavaScript stuff
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+async function callZ3(program: string): Promise<string> {
+  const { stdout, stderr } = await exe(
+    "z3 -in < " + "TODO HERE STDIN HOWTO IN NODE?"
+  );
+  console.log("stdout:", stdout);
+  console.log("stderr:", stderr);
+  return stdout;
+}
+
+/**
+ * Start up Z3 and handle requests to and from the web using Express.
+ */
+function main() {
+  const app = express();
+  const port = 3456;
+  let i = 0;
+
+  app.get("/", (req, res) => {
+    res.send(i.toString());
+    i++;
+  });
+
+  app.listen(port, () => {
+    console.log(`Example application listening at http://localhost:${port}`);
+  });
+}
+
+main();
